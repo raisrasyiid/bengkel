8527c4489090fddb68501b4d5f7beb78681642dd
raisrasyiid <raisrasyiid01@gmail.com>
2024-07-11 11:05:29 +0700

    `feat`: feature reservasi service

--- application/controllers/Booking.php
+++ application/controllers/Booking.php
@@ -32,8 +32,44 @@ class Booking extends CI_Controller {
         redirect('booking');
     }
 
-    // public function delete($id){
-    //     $this->Madmin->delete('tbl_customer', 'idCustomer', $id);
-    //     redirect('user');
-    // }
+    public function reservasi(){
+        $Btanggal = $this->input->post('tgl_book');
+        $Bjam = $this->input->post('waktu_book');
+        $keluhan = $this->input->post('keluhan');
+        $userId = $this->session->userdata('username'); 
+        
+        // Ambil data customer berdasarkan username
+        $customer = $this->db->get_where('users', array('username' => $userId))->row();
+        
+        // Pastikan data customer ditemukan
+        if (!$customer) {
+            show_error('Customer not found.', 404);
+        }
+    
+        $data = array(
+            'id_user' => $customer->id_user,
+            'keluhan' => $keluhan,
+            'tgl_book' => $Btanggal,
+            'waktu_book' => $Bjam,
+            'status' => 'Menunggu',
+            'created_at' => date('Y-m-d H:i:s'),
+        );
+
+        // Simpan data ke tbl_booking
+        if ($this->db->insert('booking', $data)) {
+            $this->session->set_flashdata('success_message', 'Booking berhasil disimpan.');
+        } else {
+            $this->session->set_flashdata('error_message', 'Terjadi kesalahan, booking gagal disimpan.');
+        }
+        
+        // Debugging
+        log_message('debug', 'Redirecting to main/profile with flashdata: ' . $this->session->flashdata('success_message'));
+
+        redirect('main');
+    }
+
+    public function delete($id){
+        $this->Model_auth->delete('booking', 'id_book', $id);
+        redirect('booking');
+    }
 }
