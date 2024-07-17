4c257a3c4f7b65521f74474a54b268de496d4599
raisrasyiid <raisrasyiid01@gmail.com>
2024-07-17 12:03:35 +0700

    `feat`: login view

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -12,11 +12,11 @@
     <title>SB Admin 2 - Login</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet');?>" type="text/css">
+    <link href="<?= base_url('assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet'); ?>" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="<?= base_url('assets/css/sb-admin-2.min.css');?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/sb-admin-2.min.css'); ?>" rel="stylesheet">
 
     <style>
         .login-container {
@@ -25,6 +25,7 @@
             justify-content: center;
             min-height: 100vh;
         }
+
         .login-card {
             max-width: 500px;
             width: 100%;
@@ -46,12 +47,12 @@
                         <!-- Card Body -->
                         <div class="p-5">
                             <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Halaman Login User!</h1>
+                                <h1 class="h4 text-gray-900 mb-4">Halaman Login Admin</h1>
                                 <?php if ($this->session->flashdata('error')) { ?>
                                     <div class="alert alert-danger"><?= $this->session->flashdata('error'); ?></div>
                                 <?php } ?>
                             </div>
-                            <form class="user" action="<?= site_url('admin/login');?>" method="POST">
+                            <form class="user" action="<?= site_url('admin/login'); ?>" method="POST">
                                 <div class="form-group">
                                     <input type="username" name="username" class="form-control form-control-user" id="exampleInputEmail" placeholder="Username">
                                 </div>
@@ -60,13 +61,13 @@
                                 </div>
                                 <div class="form-group">
                                     <button type="submit" class="btn btn-primary btn-lg btn-block">
-                                    Login
+                                        Login
                                     </button>
                                 </div>
                                 <hr>
-                            <div class="text-center">
-                                <a class="small" href="<?=site_url('admin/registerView')?>">Belum ada akun?</a>
-                            </div>
+                                <div class="text-center">
+                                    <a class="small" href="<?= site_url('admin/registerView') ?>">Belum ada akun?</a>
+                                </div>
                         </div>
                     </div>
                 </div>
@@ -77,14 +78,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?= base_url('assets/vendor/jquery/jquery.min.js');?>"></script>
-    <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js');?>"></script>
+    <script src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js');?>"></script>
+    <script src="<?= base_url('assets/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="<?= base_url('assets/js/sb-admin-2.min.js');?>"></script>
+    <script src="<?= base_url('assets/js/sb-admin-2.min.js'); ?>"></script>
 
 </body>
 
